7ba5eddce603ee6db43c1f40135202e55320b861
Rubayed <rubayedahmed04@gmail.com>
2023-09-29 11:00:14 +0600

    footer added

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,9 +30,32 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*=============== SHADOW HEADER ===============*/
-
+const shadowHeader = () => {
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewer height, add the code
+    this.scrollY >= 50 ? header.classList.add('shadow-header')
+    : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
 
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {
+    e.preventDefault()
+    emailjs.sendForm('service_3b13n0d', 'template_cehg1cr', '#contact-form', 'EltKtDlrAw-d8doJL')
+    .then(() => {
+        // show send message
+        contactMessage.textContent = "Message sent successfully ✅"
+
+        // Remove message after five second
+        setTimeout(() => {
+            contactMessage.textContent = ''
+        }, 5000)
+    })
+}
+contactForm.addEventListener('submit', sendEmail)
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
